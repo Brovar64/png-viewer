4fd8093700a215023bed720905857a658f4d30fd
Brovar64 <brovar64@gmail.com>
2025-04-11 17:43:24 +0200

    Add resizeAndPosition method to preload script

--- electron/preloadTransparent.js
+++ electron/preloadTransparent.js
@@ -20,5 +20,13 @@ contextBridge.exposeInMainWorld('transparentWindow', {
       height: Number(height),
       keepCentered: keepCentered
     }),
+  // New method to resize and position in one operation
+  resizeAndPosition: (width, height, x, y) =>
+    ipcRenderer.send('window:resizeAndPosition', {
+      width: Number(width),
+      height: Number(height),
+      x: Number(x),
+      y: Number(y)
+    }),
   close: () => ipcRenderer.send('window:close')
 });
